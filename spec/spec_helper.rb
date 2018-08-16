b1377c0beebea7124533aba69febb9115fd955ac
Joao Paulo Motta <jpaulo.motta@gmail.com>
2018-08-16 11:56:37 -0300

    Create spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 #$:.unshift File.expand_path('..', __FILE__)
 #$:.unshift File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path('..', __FILE__)
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require 'simplecov'
 SimpleCov.start
